f7da60910fe0d85e9f4fd117d12f7a6c3d47716b
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-29 19:46:09 +0100

    aboutpage - About Mobile

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import { PageHeader } from "../../components/PageHeader";
@@ -16,10 +15,86 @@ const AboutUs = () => {
 				h3Text={"Nyu's Bartending"}
 				pText={"History"}
 			></PageHeader>
-			<Link to="/">Volver</Link>
+			<div className="section">
+				<div className="section-center">
+					<div className="section-title">
+						<span>About Us</span>
+						<h2>Nyu's Bartending</h2>
+					</div>
+					{/* Paragraphs */}
+					<div className="paragraphs">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+						consequuntur praesentium voluptatibus ad exercitationem magnam qui
+						modi saepe labore earum nulla ad.
+					</div>
+					<div className="divider"></div>
+					<div className="paragraphs">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+						consequuntur praesentium voluptatibus ad exercitationem magnam qui
+						modi saepe labore earum nulla ad.
+					</div>
+					{/* CTA */}
+					<div className="cta-container">
+						<h2>Réservations</h2>
+						<p>+41 00 000 00 00</p>
+					</div>
+					{/* Images */}
+					<div className="img-container">
+						<div className="img img-left">
+							<img src={AboutUsBG} alt="About Images" />
+						</div>
+						<div className="img img-right">
+							<img src={AboutUsBG} alt="About Images" />
+						</div>
+					</div>
+				</div>
+			</div>
 		</Wrapper>
 	);
 };
 
-const Wrapper = styled.main``;
+const Wrapper = styled.main`
+	height: 100%;
+	.paragraphs {
+		margin: 0 auto;
+		font-family: var(--FontTexts);
+		letter-spacing: 0.12rem;
+		font-size: 0.87rem;
+		font-weight: 300;
+		text-align: left;
+	}
+	.divider {
+		height: 1.5rem;
+	}
+	.cta-container {
+		margin: 2.5rem 0rem;
+		font-family: var(--FontTitles);
+		letter-spacing: 0.15rem;
+		h2 {
+			font-size: 1.2rem;
+			font-weight: 300;
+			text-transform: uppercase;
+			color: var(--ColorGold);
+		}
+		p {
+			font-weight: 600;
+			letter-spacing: 0.05rem;
+			margin-top: 0.3rem;
+		}
+	}
+	.img-container {
+		height: 22rem;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		.img img {
+			margin-top: -8rem;
+			width: 8rem;
+		}
+		.img-right {
+			position: relative;
+			top: 9rem;
+		}
+	}
+`;
 export default AboutUs;
