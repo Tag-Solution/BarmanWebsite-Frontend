fc8794a99a9e7ac652687d1b120dedbf2849bd6a
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-29 14:09:18 +0100

    preloader - Arquitectura

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
+import { Preloader } from "./Preloader";
 import { Homepage } from "./Homepage";
-import DummyPage from "./DummyPage";
-import Error from "./Error";
+import { AboutUs } from "./AboutUs";
+import { Owner } from "./Owner";
+import { Features } from "./Features";
+import { Gallery } from "./Gallery";
+import { Contact } from "./Contact";
+import { Error } from "./Error";
 
-export { Homepage, Error, DummyPage };
+import DummyPage from "./DummyPage";
+
+export {
+	Preloader,
+	Homepage,
+	AboutUs,
+	Owner,
+	Features,
+	Gallery,
+	Contact,
+	Error,
+	DummyPage,
+};
