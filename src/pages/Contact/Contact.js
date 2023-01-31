7d880be0fd53351eca54c34080441a6b7c520a11
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-31 08:16:41 +0100

    contactpage - Comenzando

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import { PageHeader } from "../../components/PageHeader";
 
 import AboutUsBG from "../../assets/temp/AboutUsBG.jpg";
+import { contact_info } from "../../utils/temp/contact_info";
 
 const Contact = () => {
+	const { id, address, mobile, email, facebook, instagram, twitter, youtube } =
+		contact_info;
 	return (
-		<div>
+		<Wrapper>
 			<PageHeader
 				imgSrc={AboutUsBG}
 				imgAlt={"About Us Page"}
@@ -15,9 +18,61 @@ const Contact = () => {
 				h3Text={"Réservations"}
 				pText={"Genève, Suisse"}
 			></PageHeader>
-			<Link to="/">Volver</Link>
-		</div>
+			<div className="section">
+				<div className="section-center">
+					{/* Contact triptico */}
+					<article className="contact-info-container">
+						<div className="single-card">
+							<span>T</span>
+							<div className="info">
+								<h2>Address</h2>
+								<p>{address}</p>
+							</div>
+						</div>
+						<div className="single-card">
+							<span>T</span>
+							<div className="info">
+								<h2>Phone</h2>
+								<p>{mobile}</p>
+							</div>
+						</div>
+						<div className="single-card">
+							<span>T</span>
+							<div className="info">
+								<h2>e-mail</h2>
+								<p>{email}</p>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled.main`
+	.contact-info-container {
+		background: var(--ColorGoldLight);
+		display: flex;
+		flex-direction: column;
+		.single-card {
+			width: 90%;
+			height: 8.7rem;
+			margin: 0 auto;
+			display: grid;
+			grid-template-columns: 4rem 1fr;
+			span {
+				display: grid;
+				place-items: center;
+			}
+			.info {
+				display: flex;
+				align-items: flex-start;
+				justify-content: center;
+				flex-direction: column;
+			}
+		}
+	}
+`;
 
 export default Contact;
---
+++ src/utils/temp/contact_info.js
@@ -0,0 +1,10 @@
+export const contact_info = {
+	id: 1,
+	address: "123 Avenida Siempre Viva",
+	mobile: "+41 00 000 00 00",
+	email: "email@email.com",
+	facebook: "/facebook",
+	instagram: "@instagram",
+	twitter: "@twitter",
+	youtube: "/youtube",
+};
