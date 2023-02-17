44ac852f72b29415ff56b60e0d5fbb1e5c15fc21
Tomas Padilla <padillatomasagustin@gmail.com>
2023-02-18 00:46:37 +0100

    develop - quickfix API_CONSTANTS

--- src/utils/constants/api_constants.js
+++ src/utils/constants/api_constants.js
@@ -2,5 +2,5 @@
 export const API_BASE_URL = "https://barmanwebsite.herokuapp.com/api/v1";
 
 // Homepage:
-export const API_HOME_GET = API_BASE_URL + "/home/get";
-export const API_HOME_POST = API_BASE_URL + "/home/upload";
+export const API_HOME_GET = API_BASE_URL + "/home-page/get";
+export const API_HOME_POST = API_BASE_URL + "/home-page/create";
